80c36ef198424f2b711d7ea567ce2b191fcba108
bIropka <biropka@gmail.com>
2016-04-08 14:04:25 +0300

    final

---
+++ mail.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['name'])) {$name = $_POST['name'];}
+    if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+    if (isset($_POST['email'])) {$email = $_POST['email'];}
+    if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
+
+    $to = "biropka@gmail.com"; /*Укажите адрес, на который должно приходить письмо*/
+    $sendfrom   = "emails.in.ua"; /*Укажите адрес, с которого будет приходить письмо, можно не настоящий, нужно для формирования заголовка письма*/
+    $headers  = "From: " . strip_tags($sendfrom) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+    $subject = "$formData";
+    $message = "$formData<br> <b>Имя клиента:</b> $name <br><b>Телефон:</b> $phone <br><b>Email:</b> $email ";
+    $send = mail ($to, $subject, $message, $headers);
+    if ($send == 'true')
+    {
+    echo '<p class="success">Спасибо за обращение к нам!</p>';
+    }
+    else 
+    {
+    echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
+    }
+} else {
+    http_response_code(403);
+    echo "Попробуйте еще раз";
+}
+?>
